242d7acd9307c9f67a5ad795873db4a4623789b8
Nexe08 <rohitrautela1408@gmail.com>
2022-08-20 23:51:15 +0530

    Implemented fule limitation to box spawning machine.

--- Scripts/box_spawner.gd
+++ Scripts/box_spawner.gd
@@ -1,13 +1,30 @@
 extends Node2D
 
-onready var spawn_position = $SpawnPosition
+export var fule: float = 10
+
+
 onready var box_path = preload("res://Prefabe/Box.tscn")
+
+onready var fule_indicator = $FuleIndicator
+onready var spawn_position = $SpawnPosition
+
+const max_fule: int = 10
+
+
+func _ready() -> void:
+	fule_indicator.max_value = max_fule
+	fule_indicator.value = fule
 
 
 func spawn_box():
+	if fule <= 0:
+		return
+	
 	var instance = box_path.instance()
 	instance.global_position = spawn_position.global_position
 	get_parent().add_child(instance)
+	fule -= rand_range(1, 1.7)
+	fule_indicator.value = fule
 
 
 func start_production():
