f47a3f2e24f5695d1ea60549c76193bf3572f8f7
Nexe08 <rohitrautela1408@gmail.com>
2022-08-20 12:32:16 +0530

    Implemented element position manipulation with player's horizontal movement.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,15 +8,33 @@ var detected_box: Node = null
 var holded_box: Node = null
 
 onready var velocity: Vector2 = Vector2.ZERO
+onready var direction: int = -1 # facing direction of character
+
+onready var default_box_detector_position = $BoxDetector.position
+onready var default_box_placing_position = $BoxPlacingPosition.position
+
+onready var box_detector = $BoxDetector
 onready var box_placing_position = $BoxPlacingPosition
 onready var holded_box_position = $HoldedBoxPosition
+onready var sprite = $Sprite
 
 
 func _physics_process(delta: float) -> void:
-	velocity.x = MaxMovementSpeed * _horizontal_input_direction()
-	_handle_box_holding()
+	_handle_movement(delta)
 	_apply_gravity(delta)
+	_handle_box_holding()
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _handle_movement(dt: float):
+	velocity.x = MaxMovementSpeed * _horizontal_input_direction()
+	
+	direction = _horizontal_input_direction()
+	
+	if direction != 0:
+		sprite.scale.x = 1 * -direction
+		box_detector.position.x = default_box_detector_position.x * -direction
+		box_placing_position.position.x = default_box_placing_position.x * -direction
 
 
 func _handle_box_holding():
