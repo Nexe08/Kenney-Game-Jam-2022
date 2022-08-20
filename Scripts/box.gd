d94c7b6ac38e3e0a53020d934530659b821e736c
Nexe08 <rohitrautela1408@gmail.com>
2022-08-20 17:05:52 +0530

    Implemented physics layer to individual object. Changed box from kinematic body to rigid body.

--- Scripts/box.gd
+++ Scripts/box.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 export var Gravity: float = 200
 
@@ -10,19 +10,22 @@ onready var collision_shape = $CollisionShape2D
 
 func _physics_process(delta: float) -> void:
 	_apply_gravity(delta)
-	velocity = move_and_slide(velocity, Vector2.UP)
+#	velocity = move_and_slide(velocity, Vector2.UP)
 
 
 func _apply_gravity(dt: float) -> void:
 	if pickedup:
+		gravity_scale = 0
 		return
-	velocity.x = 0
-	velocity.y += Gravity * dt
+	gravity_scale = 1
+#	velocity.x = 0
+#	velocity.y += Gravity * dt
 
 
 # -- GLOBAL FUNC (GET CALLED IN PHYSICS PROCESS FUNC BY CALLER SCRIPT)
 func pickup(at_position: Vector2):
 	pickedup = true
+	mode = RigidBody2D.MODE_KINEMATIC
 	collision_shape.call_deferred("set_disabled", true)
 	global_position.x = lerp(global_position.x, at_position.x, 8 * get_physics_process_delta_time())
 	global_position.y = lerp(global_position.y, at_position.y, 8 * get_physics_process_delta_time())
@@ -30,6 +33,7 @@ func pickup(at_position: Vector2):
 
 func release(at_position: Vector2):
 	pickedup = false
+	mode = RigidBody2D.MODE_RIGID
 	collision_shape.call_deferred("set_disabled", false)
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, "global_position", at_position, .5)
